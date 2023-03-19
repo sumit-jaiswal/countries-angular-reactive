7521fb26d460dedac5911b34ed350fb7e05b8174
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-19 18:50:32 +0000

    add share reply to aoide multiple call

--- src/app/shared/services/countries.service.ts
+++ src/app/shared/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { COUNTRES } from 'src/app/model/countries.model';
 import { environment } from 'src/environments/environment';
 
@@ -9,11 +9,13 @@ export class CountriesService {
   constructor(private http: HttpClient) {}
 
   getCountries(): Observable<COUNTRES[]> {
-    return this.http.get<COUNTRES[]>(environment.COUNTRIES_API + '/all', {
-      params: {
-        fields:
-          'name,population,region,region,borders,tld,currencies,languages,flags,capital',
-      },
-    });
+    return this.http
+      .get<COUNTRES[]>(environment.COUNTRIES_API + '/all', {
+        params: {
+          fields:
+            'name,population,region,region,borders,tld,currencies,languages,flags,capital',
+        },
+      })
+      .pipe(shareReplay());
   }
 }
