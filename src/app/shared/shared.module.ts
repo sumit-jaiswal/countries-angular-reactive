c17be1861df3a46db6c632b722d9d0511c6eaa1d
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-19 18:06:49 +0000

    Add CountriesService in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,13 @@ import { PanelModule } from 'primeng/panel';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { CountriesService } from './services/countries.service';
+
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, PanelModule],
+  imports: [CommonModule, PanelModule, HttpClientModule],
+  providers: [CountriesService],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
