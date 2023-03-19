c4c7fd23db46ce624c1119ccc5fbfcf2f21ba417
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-19 18:50:13 +0000

    Provided loader service in root

--- src/app/shared/services/loading.service.ts
+++ src/app/shared/services/loading.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject, of } from 'rxjs';
 import { concatMap, finalize, tap } from 'rxjs/operators';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class LoadingService {
   private loadingSubject = new BehaviorSubject<boolean>(false);
 
