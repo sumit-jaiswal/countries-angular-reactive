3976f382046fbaf40008a15ae80f0585ae00de74
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-19 18:51:08 +0000

    Added loader component is share module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PanelModule } from 'primeng/panel';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { LoadingComponent } from './loading/loading.component';
 
-import { HttpClientModule } from '@angular/common/http';
 import { CountriesService } from './services/countries.service';
+import { LoadingService } from './services/loading.service';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, PanelModule, HttpClientModule],
+  declarations: [HeaderComponent, FooterComponent, LoadingComponent],
+  imports: [CommonModule, PanelModule, HttpClientModule, ProgressSpinnerModule],
   providers: [CountriesService],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, LoadingComponent],
 })
 export class SharedModule {}
