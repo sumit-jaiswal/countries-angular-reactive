3b9e25182da52650d99bfb41ac6eae00c1d3c252
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-20 08:21:39 +0000

    Added some utilis fuction

--- src/app/shared/utils/country-filter.util.ts
+++ src/app/shared/utils/country-filter.util.ts
@@ -3,3 +3,7 @@ import { Country } from 'src/app/model/countries.model';
 export function sortByPopulation(country1: Country, country2: Country) {
   return country2.population - country1.population;
 }
+
+export function filterByRegion(country: Country, region: string) {
+  return country.region == region;
+}
