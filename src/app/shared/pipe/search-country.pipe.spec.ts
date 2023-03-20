0056dbc991a2f65bf02315254e66928b8fad04db
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-20 17:12:28 +0000

    Write a unit test cases for SearchCountry pipe

--- src/app/shared/pipe/search-country.pipe.spec.ts
+++ src/app/shared/pipe/search-country.pipe.spec.ts
@@ -1,8 +1,19 @@
+import { COUNTRIES_MOCK } from 'src/app/testing/countries.mock';
 import { SearchCountryPipe } from './search-country.pipe';
 
-describe('SearchCountryPipe', () => {
-  it('create an instance', () => {
+fdescribe('SearchCountryPipe', () => {
+  it('Create an instance', () => {
     const pipe = new SearchCountryPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Search country by name', () => {
+    const searchCountryPipe = new SearchCountryPipe();
+    expect(searchCountryPipe.transform(COUNTRIES_MOCK, 'india').length).toBe(1);
+  });
+
+  it('If search string Empty then no action perform and return same array', () => {
+    const searchCountryPipe = new SearchCountryPipe();
+    expect(searchCountryPipe.transform(COUNTRIES_MOCK, '').length).toBe(3);
   });
 });
