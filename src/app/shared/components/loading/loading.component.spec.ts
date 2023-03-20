2780c35574dbc2d5a03636a76cad15b4ac507a2e
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-03-20 17:53:42 +0000

    write unit test case for loading component

--- src/app/shared/components/loading/loading.component.spec.ts
+++ src/app/shared/components/loading/loading.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoadingService } from '../../services/loading.service';
 import { LoadingComponent } from './loading.component';
 
 describe('LoadingComponent', () => {
   let component: LoadingComponent;
   let fixture: ComponentFixture<LoadingComponent>;
+  let loader: LoadingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,10 +14,19 @@ describe('LoadingComponent', () => {
 
     fixture = TestBed.createComponent(LoadingComponent);
     component = fixture.componentInstance;
+    loader = TestBed.inject(LoadingService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display loader', () => {
+    const fixture = TestBed.createComponent(LoadingComponent);
+    loader.loadingOn();
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('progress-spinner')).toBeDefined();
   });
 });
